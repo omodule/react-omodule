e74b5059492f44ce8ad909d80f23b94cf149b74d
wuchangming <wuchangmingnice@163.com>
2018-08-20 21:12:31 +0800

    update examples

--- examples/tree-structure-demo/src/App.js
+++ examples/tree-structure-demo/src/App.js
@@ -2,11 +2,11 @@ import React from 'react'
 export default () => (
     <div>
         <h4>omodule 树状结构</h4>
-        <p>
+
             <a href="#counter-parent-a">
                 <h4>跳转到counter-parent-a</h4>
             </a>
-        </p>
+
         <p>
             <a href="#counter-parent-a/counter-child-a">
                 跳转到 counter-parent-a 下的「子模块」counter-child-a
@@ -17,11 +17,11 @@ export default () => (
                 跳转到 counter-parent-a 下的「子模块」counter-child-b
             </a>
         </p>
-        <p>
+
             <a href="#counter-parent-b">
                 <h4>跳转到counter-parent-b</h4>
             </a>
-        </p>
+        
         <p>
             <a href="#counter-parent-b/counter-child-a">
                 跳转到 counter-parent-b 下的「子模块」counter-child-a
